c44c76f8b1ba03359bce2d50047545f888d0429e
Pasha <9139136@gmail.com>
2021-04-05 00:07:47 +0300

    Hw2 and little part Hw3

---
+++ Lesson_2_basic/hw_2_1.py
@@ -0,0 +1,11 @@
+# Найдите в программе необъявленную переменную и объявите её, присвоив ей значение ‘Кот’.
+# client = 'Петя'
+# print(client)
+# print(' и ')
+# print(pet)
+
+pet = 'Кот'
+client = 'Петя'
+print(client)
+print(' и ')
+print(pet)
