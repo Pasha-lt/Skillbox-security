5f631889e7516ec77bd874644b8956af32cac467
Pasha <9139136@gmail.com>
2021-04-23 18:22:50 +0300

    fix output hw_9_3

--- lesson_09/hw_9_03.py
+++ lesson_09/hw_9_03.py
@@ -21,7 +21,7 @@ for symbol in users_string:
     counter += 1
     print(symbol)
     if symbol == '*':
-        print(counter)
+        print(f'Символ ‘*’ стоит на позиции {counter}')
         break
 else:
     print('В сообщении нету знака "*"')
