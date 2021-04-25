e962634b16bfb5bd2b5455ab47dc6e86f75ccdf8
Pasha <9139136@gmail.com>
2021-04-25 12:32:03 +0300

    fix 8_9

--- lesson_08/hw_8_09.py
+++ lesson_08/hw_8_09.py
@@ -4,8 +4,6 @@
 
 x = int(input("Введите x: "))
 final_score = 1
-
-for number in range(1, 65, 2):
-    final_score *= (x - number) / (x - number + 1)
-
+for number in range(1, 7):
+    final_score *= (x - ((2 ** number) - 1)) / (x - (2 ** number))
 print(final_score)
