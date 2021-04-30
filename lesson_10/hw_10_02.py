9fa1d92bd07e69fc2adf90ae16c97be00df986bb
Pasha <9139136@gmail.com>
2021-04-30 22:31:23 +0300

    hw_10_02skillbox

--- lesson_10/hw_10_02.py
+++ lesson_10/hw_10_02.py
@@ -9,3 +9,13 @@ print('Задача 2. Лестница')
 # 3 3 3
 # 4 4 4 4
 # 5 5 5 5 5
+
+
+users_number = int(input('Введите число: '))
+for row in range(users_number + 1):
+    for mult in range(row):
+        print(row, end=' ')
+    print()
+
+# for row in range(users_number+1):
+#     print(str(row) * row)
