89f89e73f00351eabbcb1d37f4eaa0d906e970c2
Pasha <9139136@gmail.com>
2021-05-21 18:41:30 +0300

    13_05

--- Python_basic/lesson_13/hw_13_05.py
+++ Python_basic/lesson_13/hw_13_05.py
@@ -8,3 +8,18 @@ print('Задача 5. Число Эйлера')
 
 # Точность: 1e-20
 # Результат: 2.7182818284590455
+
+from math import factorial
+
+precision = float(input('Точность: '))
+
+i = 0
+result = 0
+addMember = 1
+
+while addMember > precision:
+    addMember = 1 / factorial(i)
+    i += 1
+    result += addMember
+
+print(f'Результат: {result}')
