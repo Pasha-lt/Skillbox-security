0b15677eddd8165b2dcad4b48af8106ab517a709
Pasha-lt <9139136@gmail.com>
2021-09-14 21:43:10 +0300

    hw_19_10 14_09_2021

---
+++ Python_basic/lesson_19/hw_19_10.py
@@ -0,0 +1,32 @@
+'''
+Задача 10 (по желанию). Снова палиндром
+Пользователь вводит строку. Необходимо написать программу, которая определяет,
+существует ли у этой строки такая перестановка, при которой она станет палиндромом.
+Выведите соответствующее сообщение.
+
+Пример 1:
+Введите строку: aab
+Можно сделать палиндромом
+
+Пример 2:
+Введите строку: aabc
+Нельзя сделать палиндромом
+'''
+
+
+def is_polyndrom(us_string):
+    char_dict = {}
+    for i_sym in us_string:
+        char_dict[i_sym] = char_dict.get(i_sym, 0) + 1
+    
+    odd_count = 0
+    for i_value in char_dict.values():
+        if i_value % 2 != 0:
+            odd_count += 1
+    
+    return odd_count <= 1
+
+
+assert is_polyndrom('aab') == True
+assert is_polyndrom('aaab') == False
+assert is_polyndrom('aabbc') == True
