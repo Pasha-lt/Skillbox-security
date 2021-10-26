70f0491e4e42636ff1f4f40c594b80f37de2449c
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2021-10-26 22:10:43 +0300

    Update hw_17_07.py

--- Python_basic/lesson_17/hw_17_07.py
+++ Python_basic/lesson_17/hw_17_07.py
@@ -16,5 +16,22 @@ list_1 = [[1] for i in range(1,5)]
 list_3 = [[1] for i in range(1,5)]
 [list_3[i].append((j+1)*4+i+list_3[i][0]) for i in range(4) for j in range(2)]
 
+
+# first solution
+list_1 = [[1] for i in range(1, 5)]
+[list_1[i].append((i) + 4 + list_1[i][0]) for i in range(4)]
+[list_1[i].append((i) + 8 + list_1[i][0]) for i in range(4)]
+
+# second solution
+list_3 = [[1] for i in range(1, 5)]
+[list_3[i].append((j + 1) * 4 + i + list_3[i][0]) for i in range(4) for j in range(2)]
+
+
+# third solution
+new_list = [[elem for elem in range(n, n + 12, 4)] for n in range(1, 5)]
+print(new_list)
+
+
+
 print(list_1)
 print(list_3)
