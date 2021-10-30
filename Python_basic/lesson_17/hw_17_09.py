f38850e97da350e4d21de1aa8805154fc4b4f1a8
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2021-10-30 22:19:46 +0300

    Update hw_17_09.py

--- Python_basic/lesson_17/hw_17_09.py
+++ Python_basic/lesson_17/hw_17_09.py
@@ -10,3 +10,7 @@ nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16
 nice_list_2 = [nice_list[j][x][y] for j in range(2) for x in range(3) for y in range(3)]
 print(nice_list)
 print(nice_list_2)
+
+nice_list_1 = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+nice_list_21 = [z for x in nice_list_1 for y in x for z in y]
+print(nice_list_21)
